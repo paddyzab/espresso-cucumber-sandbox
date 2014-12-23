d09b1b2be5acdca3cef50e10919699f8082df415
Patryk Zabicki <patryk.zabicki@gmail.com>
2014-12-23 09:42:57 +0100

    Working coverage for LoginActivity.

--- app/src/androidTestUats/assets/features/login.feature
+++ app/src/androidTestUats/assets/features/login.feature
@@ -2,4 +2,11 @@
 Feature: Login
 
     Scenario: Perform login with the username and password
+        When I type as login paddy@zab.com
+        Then I type as password qwerty12
+        Then I click on login button
+
+    Scenario: Display error when user is not using email as login
+        When I type as login paddy
+        Then I type as password qwerty12
         Then I click on login button
--- app/src/androidTestUats/java/com/paddy/cuespton/cuespton/test/steps/EditTextSteps.java
+++ app/src/androidTestUats/java/com/paddy/cuespton/cuespton/test/steps/EditTextSteps.java
@@ -1,4 +1,26 @@
 package com.paddy.cuespton.cuespton.test.steps;
 
+import com.paddy.cuespton.cuespton.R;
+import cucumber.api.java.en.Then;
+import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView;
+import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.clearText;
+import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.typeText;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withId;
+
 public class EditTextSteps {
+
+    @Then("^I type as login (\\S+)")
+    public static void I_type_as_login(final String login) {
+        onView(withId(R.id.editTextLogin)).perform(clearText(), typeText(login));
+    }
+
+    @Then("^I type as password (\\S+)")
+    public static void I_type_as_password(final String password) {
+        onView(withId(R.id.editTextPassword)).perform(clearText(), typeText(password));
+    }
+
+    @Then("^I expect error on login input field (\\S+)")
+    public static void I_expect_error_on_login_input_field(final String errorMessage) {
+        //onView(withId(R.id.editTextLogin)).check()
+    }
 }
